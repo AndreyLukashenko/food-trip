0ca0157b20cd7a5c3a6866bdb0d224e6694a2e01
Andrei Lukashenka <LukashenkoAndrey@gmail.com>
2018-04-01 23:14:32 +0300

    Add tslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,6 @@ const app = new Koa();
 
 app.use(routes);
 
-app.listen(3000);
-
-console.log('Server running on port 3000');
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
